e911a2ec87df6eabc8023b08eff7f60318460b8f
wwatanabe331 <wwatanabe331@gmail.com>
2025-07-02 22:42:28 +0900

    SubPageの骨組み

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import AppLayout from "./components/layout/AppLayout";
 import MainPage from "./pages/MainPage";
+import SubPage from "./pages/SubPage";
 
 function App() {
   const [activeSlide, setActiveSlide] = useState(0);
   const [monthRange, setMonthRange] = useState({ start: 0, end: 11 });
 
   return (
-    <AppLayout monthRange={monthRange} onMonthChange={setMonthRange}>
+    <AppLayout
+      monthRange={monthRange}
+      onMonthChange={setMonthRange}
+      rightContent={<SubPage />}
+    >
       <MainPage
         activeSlide={activeSlide}
         setActiveSlide={setActiveSlide}
---
+++ src/pages/SubPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const SubPage = () => {
+  return (
+    <div>
+      <h1>ふ</h1>
+    </div>
+  );
+};
+
+export default SubPage;
