bb91eee73db8b460d714aa44f08520c64ee2ae0d
wwatanabe331 <wwatanabe331@gmail.com>
2025-07-10 13:50:45 +0900

    一旦

--- src/pages/FlowersCart.jsx
+++ src/pages/FlowersCart.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/cart.css";
 
 const FlowersCart = () => {
   return (
     <div>
-      <div>umu</div>
-      <button>グリーン系を選択</button>
-      <button>花束を作成する</button>
+      <header className="header">
+        <Link to="/" className="back-button">
+          花束作成支援サイト
+        </Link>
+      </header>
+
+      <div className="cart-content">
+        <div>umu</div>
+
+        <div></div>
+        <button className="green-modal">グリーン系を選択</button>
+        <button className="create-button">作成</button>
+      </div>
     </div>
   );
 };
